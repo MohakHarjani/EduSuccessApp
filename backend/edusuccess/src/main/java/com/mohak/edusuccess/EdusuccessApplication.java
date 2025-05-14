6bd755101bdac1d5ed7cb35a1dc4069418c16638
MohakHarjani <mh2001harjani@gmail.com>
2025-05-14 12:42:04 +0530

    initialized repo

---
+++ backend/edusuccess/src/main/java/com/mohak/edusuccess/EdusuccessApplication.java
@@ -0,0 +1,15 @@
+package com.mohak.edusuccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusuccessApplication {
+
+//	Started coding this application
+	public static void main(String[] args) {
+
+		SpringApplication.run(EdusuccessApplication.class, args);
+	}
+
+}
---
+++ backend/edusuccess/src/test/java/com/mohak/edusuccess/EdusuccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohak.edusuccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusuccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
